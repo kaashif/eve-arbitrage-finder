df26766dcffd60673415b019573a0d75bc7b47cc
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2023-09-26 00:21:33 -0400

    Support multiple filenames

--- convert_file.py
+++ convert_file.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python3.11
 from io import StringIO
+from datetime import datetime
 
 import pandas as pd
 import bz2
 import sys
 
-csv_bz2_filename = sys.argv[1]
-parquet_filename = csv_bz2_filename.replace(".csv.bz2", ".parquet")
+csv_bz2_filenames = sys.argv[1:]
+num_files = len(csv_bz2_filenames)
 
-with bz2.open(csv_bz2_filename, mode="r") as data_csv:
-    contents = StringIO(bytes.decode(data_csv.read(), "utf-8"))
+print(f"{num_files=}")
 
-df = pd.read_csv(contents)
-df.to_parquet(parquet_filename, compression="gzip")
+file_num = 0
+
+for csv_bz2_filename in csv_bz2_filenames:
+    start = datetime.now()
+    print(f"{file_num=}: ", end="")
+    file_num += 1
+
+    parquet_filename = csv_bz2_filename.replace(".csv.bz2", ".parquet")
+
+    with bz2.open(csv_bz2_filename, mode="r") as data_csv:
+        contents = StringIO(bytes.decode(data_csv.read(), "utf-8"))
+
+    df = pd.read_csv(contents)
+    df.to_parquet(parquet_filename, compression="gzip")
+    end = datetime.now()
+    time_taken = end-start
+
+    time_left = (num_files - file_num) * time_taken
+    print(time_left)
--- find_best_arb.py
+++ find_best_arb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.11
 
+import sys
 import bz2
 import csv
 import datetime
@@ -21,11 +22,8 @@ def read_jump_graph():
 
 G = read_jump_graph()
 
-start = datetime.datetime.now()
 
-def read_order_file():
-    filename = "data.everef.net/market-orders/history/2023/2023-01-01/market-orders-2023-01-01_00-15-03.v3.csv.bz2"
-
+def read_order_file(filename):
     with bz2.open(filename, mode="r") as data_csv:
         contents = data_csv.read()
 
@@ -33,64 +31,16 @@ def read_order_file():
     reader = csv.DictReader(contents_f)
     return reader
 
-reader = read_order_file()
-
-type_id_to_buys_and_sells = {}
-
-for row in reader:
-    row["price"] = float(row["price"])
-    row["volume_remain"] = int(row["volume_remain"])
-    row["min_volume"] = int(row["min_volume"])
-    type_id = row["type_id"]
-    if type_id not in type_id_to_buys_and_sells:
-        buys_and_sells = {"buys": [], "sells": []}
-        type_id_to_buys_and_sells[type_id] = buys_and_sells
-    else:
-        buys_and_sells = type_id_to_buys_and_sells[type_id]
-
-    if row["is_buy_order"] == 'true':
-        buys_and_sells["buys"].append(row)
-    else:
-        buys_and_sells["sells"].append(row)
-
-arbitrages = []
-
-for type_id, buys_and_sells in type_id_to_buys_and_sells.items():
-    sells = buys_and_sells["sells"]
-    buys = buys_and_sells["buys"]
-
-    if len(sells) == 0 or len(buys) == 0:
-        continue
-
-    # We only need consider the order pairs that could possibly yield a profit
-    # i.e. buy orders higher than the min sell price and sell orders lower than the max buy price.
-    min_sell_price = min(sell["price"] for sell in sells)
-    max_buy_price = max(buy["price"] for buy in buys)
-
-    good_sells = (sell for sell in sells if sell["price"] < max_buy_price)
-    good_buys = (buy for buy in buys if buy["price"] > min_sell_price)
-
-    for buy in good_buys:
-        for sell in good_sells:
-            # conditions for valid arbitrage:
-            # 1. sell order lower than buy order
-            # (i.e. we buy from the sell order low, and sell into the buy order high)
-            # 2. sell volume remain is higher than buy min_volume
-            if sell["price"] < buy["price"] and sell["volume_remain"] > buy["min_volume"]:
-                # arbitrage found!
-                arbitrages.append((sell, buy))
-
-
 def arbitrage_stats(arbitrage, wallet_amount):
     max_quantity = min(arbitrage[0]["volume_remain"], arbitrage[1]["volume_remain"])
     sell_price = arbitrage[0]["price"]
     buy_price = arbitrage[1]["price"]
 
     # you can only invest as much as you can afford
-    quantity = min(max_quantity, wallet_amount / sell_price)
+    quantity = min(max_quantity, wallet_amount // sell_price)
     real_investment = sell_price * quantity
     profit = quantity * (buy_price - sell_price)
-    total_return = 100 * profit / real_investment
+    total_return = 0 if real_investment == 0 else 100 * profit / real_investment
 
     # If you have money in your wallet you can't invest because the opportunity is too small
     # then it's like you did invest it, but with no return.
@@ -113,38 +63,107 @@ def arbitrage_stats(arbitrage, wallet_amount):
         "profit": profit
     }
 
-def get_sorted_arb_stats(wallet_amount):
+def get_sorted_arb_stats(arbitrages, wallet_amount):
     return sorted([arbitrage_stats(arb, wallet_amount) for arb in arbitrages], key=lambda arb: arb["adj_return"], reverse=True)
 
-wallet_amt_to_arb_stats = {k:get_sorted_arb_stats(k) for k in [1_000_000, 10_000_000, 100_000_000, 1_000_000_000]}
 
-# We need to annotate each arbitrage with its distance
-amt_to_valid_arbitrages = {amt: [] for amt in wallet_amt_to_arb_stats.keys()}
+def get_best_arb(filename, index):
 
-for amt, arbs in wallet_amt_to_arb_stats.items():
-    for arb in arbs:
-        if not G.has_node(arb["from_system"]) or not G.has_node(arb["to_system"]):
-            break
-        if not nx.has_path(G, arb["from_system"], arb["to_system"]):
-            # There are a lot of arbitrages in this list between
-            # different universes and stuff like that
-            break
+    reader = read_order_file(filename)
 
-        arb["jumps"] = len(nx.shortest_path(G, arb["from_system"], arb["to_system"]))
+    type_id_to_buys_and_sells = {}
 
-        # return per jump, considering compounding
-        # i.e. a 100% return per jump with 2 jumps means you double your money
-        # twice for a total of 300% return for the whole trip
+    for row in reader:
+        row["price"] = float(row["price"])
+        row["volume_remain"] = int(row["volume_remain"])
+        row["min_volume"] = int(row["min_volume"])
+        type_id = row["type_id"]
+        if type_id not in type_id_to_buys_and_sells:
+            buys_and_sells = {"buys": [], "sells": []}
+            type_id_to_buys_and_sells[type_id] = buys_and_sells
+        else:
+            buys_and_sells = type_id_to_buys_and_sells[type_id]
 
-        arb["adj_return_per_jump"] = 100 * ((1 + (arb["adj_return"] / 100)) ** (1 / arb["jumps"]) - 1)
+        if row["is_buy_order"] == 'true':
+            buys_and_sells["buys"].append(row)
+        else:
+            buys_and_sells["sells"].append(row)
 
-        amt_to_valid_arbitrages[amt].append(arb)
+    arbitrages = []
 
-    amt_to_valid_arbitrages[amt].sort(key=lambda a: a["adj_return_per_jump"], reverse=True)
+    for type_id, buys_and_sells in type_id_to_buys_and_sells.items():
+        sells = buys_and_sells["sells"]
+        buys = buys_and_sells["buys"]
 
-for amt, arbs in amt_to_valid_arbitrages.items():
-    print(str(amt) + "," + str(arbs[0]["adj_return_per_jump"]))
+        if len(sells) == 0 or len(buys) == 0:
+            continue
 
-end = datetime.datetime.now()
+        # We only need consider the order pairs that could possibly yield a profit
+        # i.e. buy orders higher than the min sell price and sell orders lower than the max buy price.
+        min_sell_price = min(sell["price"] for sell in sells)
+        max_buy_price = max(buy["price"] for buy in buys)
 
-print(end-start)
+        good_sells = (sell for sell in sells if sell["price"] < max_buy_price)
+        good_buys = (buy for buy in buys if buy["price"] > min_sell_price)
+
+        for buy in good_buys:
+            for sell in good_sells:
+                # conditions for valid arbitrage:
+                # 1. sell order lower than buy order
+                # (i.e. we buy from the sell order low, and sell into the buy order high)
+                # 2. sell volume remain is higher than buy min_volume
+                if sell["price"] < buy["price"] and sell["volume_remain"] > buy["min_volume"]:
+                    # arbitrage found!
+                    arbitrages.append((sell, buy))
+
+
+    wallet_amt_to_arb_stats = {k:get_sorted_arb_stats(arbitrages, k) for k in [1_000_000, 10_000_000, 100_000_000, 1_000_000_000]}
+
+    # We need to annotate each arbitrage with its distance
+    amt_to_valid_arbitrages = {amt: [] for amt in wallet_amt_to_arb_stats.keys()}
+
+    for amt, arbs in wallet_amt_to_arb_stats.items():
+        for arb in arbs:
+            if not G.has_node(arb["from_system"]) or not G.has_node(arb["to_system"]):
+                break
+            if not nx.has_path(G, arb["from_system"], arb["to_system"]):
+                # There are a lot of arbitrages in this list between
+                # different universes and stuff like that
+                break
+
+            arb["jumps"] = len(nx.shortest_path(G, arb["from_system"], arb["to_system"]))
+
+            # return per jump, considering compounding
+            # i.e. a 100% return per jump with 2 jumps means you double your money
+            # twice for a total of 300% return for the whole trip
+
+            arb["adj_return_per_jump"] = 100 * ((1 + (arb["adj_return"] / 100)) ** (1 / arb["jumps"]) - 1)
+
+            amt_to_valid_arbitrages[amt].append(arb)
+
+        amt_to_valid_arbitrages[amt].sort(key=lambda a: a["adj_return_per_jump"], reverse=True)
+
+    for amt, arbs in amt_to_valid_arbitrages.items():
+        print(str(index) + "," + str(amt) + "," + str(arbs[0]["adj_return_per_jump"] + "," + str(arbs[0]["adj_return"])))
+
+
+
+parquet_filenames = sys.argv[1:]
+
+num_files = len(parquet_filenames)
+
+print(f"{num_files=}", file=sys.stderr)
+
+file_num = 0
+
+for parquet_filename in parquet_filenames:
+    print(f"{file_num=}: ", end="", file=sys.stderr)
+    file_num += 1
+
+    start = datetime.datetime.now()
+    get_best_arb(parquet_filename, file_num)
+    end = datetime.datetime.now()
+    time_taken = end-start
+
+    time_left = (num_files - file_num) * time_taken
+    print(time_left, file=sys.stderr)
