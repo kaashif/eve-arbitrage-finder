5a7e9f21f19f9ff884a1594741f6172d8bdffb34
kaashif <kaashif@kaashif.co.uk>
2024-12-09 22:39:55 -0500

    Add more data downloads

--- download.sh
+++ download.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 wget -r https://data.everef.net/market-orders/history/2023/2023-01-01/ --accept-regex='.*bz2'
+wget https://www.fuzzwork.co.uk/dump/sde-20241112-TRANQUILITY/mapSolarSystemJumps.csv
+wget https://www.fuzzwork.co.uk/dump/sde-20241112-TRANQUILITY/invTypes.csv
